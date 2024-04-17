562abe9a16c07476e1225c5426c35115d701acf2
Matthias Van Woensel <matthias.vanwoensel@platform.sh>
2024-04-17 18:20:59 +0200

    Adding code

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # Pedal Mapper
 
 This is a simple program for mapping the buttons on an Elgato Stream Deck Pedal
-to keyboard inputs. I use a pedal for push-to-talk when I play games with a
-controller, and there wasn't anything that let me do that easily with the Elgato
-Stream Deck Pedal.
+to keyboard inputs. 
 
 ## Dependencies
 
@@ -14,51 +12,49 @@ There aren't a lot of dependencies, but what you do need are:
 
 ## How it works
 
-The actual flow is really simple. You just configure three lists of key presses,
-one for each button on the pedal, and then run the program. The program then
-listens for button presses and sends the mapped keys to `/dev/uinput`.
-
-Run the Python script and press your pedal. You can run it as a `systemd` unit
+Run the Python script with `sudo python3 pedal_mapper.py` and press your pedal. You can run it as a `systemd` unit
 or equivalent to have it always running in the background.
 
 You can find a list of all possible keycodes
 [here](https://github.com/torvalds/linux/blob/master/include/uapi/linux/input-event-codes.h).
 
-## Possible issues
 
-If you're using Wayland it won't work if your voice chat client isn't focused.
-However, if you have a means of forwarding keystrokes to Xwayland applications
-and your client is running in Xwayland, then it will work fine. KDE has this
-built-in and that's what I use. There are other solutions like [this
-script](https://github.com/Rush/wayland-push-to-talk-fix/) for forwarding
-keystrokes.
+## Configuring
 
-There may be other issues. I haven't really run into any myself yet, but you
-never know.
+Configuring is done in the python code itself at the moment. This config will allow you to:
 
-## Possible additions
+- double press the middle button to ALT+TAB to the next window
+- single press the middle button to press ALT and automatically holds it. You can then press the right and left button to navigate windows. Press ALT again to select.
+- double press the right button to send CTRL+W, which generally closes tabs
 
-This was just my first pass at implementing this specifically for my needs. The
-code sucks, is basic as hell, and isn't very configurable. In no particular
-order, things that I may implement in the future if I feel inspired are:
-- Reading configuration from a file rather than being hardcoded into the
-  program.
-- Configuring other types of devices.
-  [input-remapper](https://github.com/sezanzeb/input-remapper) is already a good
-  general purpose solution, but it's possible there are unsupported devices for
-  which people might want a similar solution to what is here.
-- More advanced key combinations / macros. Right now the program just handles
-  basic hold keys and press keys. Adding something like callbacks to associate
-  with buttons could be neat, with a default one being how it works right now.
-- Profiles/profile-switching would be neat. Being able to switch stuff on the
-  fly would be handy if someone wanted to use different bindings for different
-  situations.
+```
 
-These are all highly aspirational and I don't actually need any of these myself
-currently. I'm lazy and selfish so it's actually unlikely I'll implement these,
-but they're here as ideas in case anyone else wants to contribute.
+    pm = PedalMapper(
+        left_keys=[KeyCombo(mods=[e.KEY_LEFTSHIFT, e.KEY_TAB])],
+        middle_keys=[KeyCombo(mods=[e.KEY_LEFTALT], hold=True)], # press ALT and hold it so we can select the right screen
+        right_keys=[KeyCombo(mods=[e.KEY_TAB])],
+        #dbl_left_keys=[KeyCombo(mods=[e.KEY_LEFTCTRL], keys=[e.KEY_A])], # select all
+        dbl_right_keys=[KeyCombo(mods=[e.KEY_LEFTCTRL], keys=[e.KEY_W])], # close window
+        dbl_middle_keys=[KeyCombo(mods=[e.KEY_LEFTALT, e.KEY_TAB])], # press ALT and TAB to switch between screens immediately
+    )
+```
 
-## Contributing
+## Systemd
 
-Feel free to submit merge requests. I'm not too scary and would happily accept
-contributions.
+```
+cat systemd/system/pedal.service 
+[Unit]
+Description=Pedal service
+After=network.target
+
+[Service]
+StartLimitIntervalSec=0
+Type=simple
+Restart=always
+RestartSec=1
+User=root  
+ExecStart=/usr/bin/python3 /home/matthiasvanwoensel/PedalMapper/pedal_mapper.py
+
+[Install]
+WantedBy=multi-user.target
+```
--- pedal_mapper.py
+++ pedal_mapper.py
@@ -2,6 +2,8 @@ from collections import namedtuple
 from enum import Enum
 from evdev import UInput, ecodes as e
 import hid
+from collections import deque 
+from threading import Timer
 
 # Elgato Stream Deck Pedal Vendor / Product IDs
 VENDOR_ID = 0x0FD9
@@ -10,7 +12,7 @@ PRODUCT_ID = 0x0086
 
 # Represents a key combination of mods, which are held for the whole
 # combination, and keys, which are pressed and released.
-KeyCombo = namedtuple("KeyCombo", ["mods", "keys"], defaults=([], []))
+KeyCombo = namedtuple("KeyCombo", ["mods", "keys", "hold"], defaults=([], []))
 
 
 # Simple enum that represents the three buttons.
@@ -18,16 +20,19 @@ class Button(Enum):
     LEFT = 0
     MIDDLE = 1
     RIGHT = 2
-
+    
+sequences = deque([],maxlen=2) 
+holds = []
 
 class PedalMapper:
-    def __init__(self, left_keys=[], middle_keys=[], right_keys=[], polling_rate=10):
-        self.button_key_mappings = (left_keys, middle_keys, right_keys)
+    def __init__(self, left_keys=[], middle_keys=[], right_keys=[], dbl_left_keys=[], dbl_middle_keys=[], dbl_right_keys=[], polling_rate=10):
+        self.button_key_mappings = (left_keys, middle_keys, right_keys, dbl_left_keys, dbl_middle_keys, dbl_right_keys)
         self.button_state = [False, False, False]
         self.polling_rate = polling_rate
         self.dev = hid.device()
         self.dev.open(VENDOR_ID, PRODUCT_ID)
         self.dev.set_nonblocking(1)
+        self.t = None
 
         # Register keys in UInput capabilities.
         reg_keys = set()
@@ -37,6 +42,13 @@ class PedalMapper:
                 reg_keys.update(combo.keys)
         cap = {e.EV_KEY: reg_keys}
         self.ui = UInput(cap, name="Pedal Mapper Virtual Input")
+
+    def clear_sequences_and_send_value(self, value):
+        sequences.clear()
+        self.btn_value_to_keys(value)
+
+    def newTimer(self, value):
+        self.t = Timer(0.3, self.clear_sequences_and_send_value, [value])
 
     def get_event(self):
         # We're non-blocking, so wait for a poll. This could just be changed to
@@ -54,24 +66,59 @@ class PedalMapper:
             button = Button.RIGHT
         else:
             return
+        
+        
         self.button_state[button.value] = not self.button_state[button.value]
-        return button, self.button_state[button.value]
+        
+        if self.button_state[button.value]: # when we let go of the pedal it will send back the button
+            return button
 
-    def handle_key(self, button, state):
-        key_combos = self.button_key_mappings[button.value]
+
+    def handle_key(self, button):
+        # keep a sequence of buttons (just to detect double press but I guess could be used to program sequences?)
+        sequences.appendleft(button)
+        if self.t:
+            self.t.cancel()
+
+        if len(sequences) > 1 and sequences[0] and sequences[0] == sequences[1]:
+            # double pressed the same button
+            self.clear_sequences_and_send_value(button.value + 3)
+        else:
+            # else: not a double press, we need to wait until the timer expires, which is 0.5 seconds
+            self.newTimer(button.value)
+            self.t.start()
+        
+                
+    def btn_value_to_keys(self, value):
+        # record which one is pressed
+        key_combos = self.button_key_mappings[value]
         for combo in key_combos:
-            # Press mods first
-            if state:
-                for mod in combo.mods:
-                    self.write_key(mod, state)
+            # press            
+            for mod in combo.mods:
+                if combo.hold and mod in holds:
+                    # if we already pressed the hold, release it
+                    self.release(mod)
+                    holds.remove(mod)
+                else:
+                    if combo.hold and mod not in holds:
+                        holds.append(mod)
+                    self.press(mod)
+            
+            for key in combo.keys:
+                self.press(key)
+                
+            # release (only do if the combo is not a 'hold')
+            if not combo.hold:
                 for key in combo.keys:
-                    self.write_key(key, state)
-            # Release mods last
-            else:
-                for key in combo.keys:
-                    self.write_key(key, state)
+                    self.release(key)
                 for mod in combo.mods:
-                    self.write_key(mod, state)
+                    self.release(mod)
+
+    def press(self, key):
+        self.write_key(key, True)
+
+    def release(self,  key):
+        self.write_key(key, False)
 
     # Writes a key to uinput and then syncs.
     def write_key(self, key, state):
@@ -81,19 +128,17 @@ class PedalMapper:
 
 if __name__ == "__main__":
     # Create Pedal
-    # Example mappings:
-    #   Left: no-op
-    #   Middle: right meta + F13
-    #   Right: shift + right meta + F13
     pm = PedalMapper(
-        middle_keys=[KeyCombo(mods=[e.KEY_RIGHTMETA], keys=[e.KEY_F13])],
-        right_keys=[
-            KeyCombo(mods=[e.KEY_RIGHTSHIFT, e.KEY_RIGHTMETA], keys=[e.KEY_F13])
-        ],
+        left_keys=[KeyCombo(mods=[e.KEY_LEFTSHIFT, e.KEY_TAB])],
+        middle_keys=[KeyCombo(mods=[e.KEY_LEFTALT], hold=True)], # press ALT and hold it so we can select the right screen
+        right_keys=[KeyCombo(mods=[e.KEY_TAB])],
+        #dbl_left_keys=[KeyCombo(mods=[e.KEY_LEFTCTRL], keys=[e.KEY_A])], # select all
+        dbl_right_keys=[KeyCombo(mods=[e.KEY_LEFTCTRL], keys=[e.KEY_W])], # close window
+        dbl_middle_keys=[KeyCombo(mods=[e.KEY_LEFTALT, e.KEY_TAB])], # press ALT and TAB to switch between screens immediately
     )
 
     # Loop to get events and handle them accordingly.
     while True:
         ev = pm.get_event()
         if ev:
-            pm.handle_key(ev[0], ev[1])
+            pm.handle_key(ev)
